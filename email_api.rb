2953cce3da58fda76082611a2adb3db5b55d63fe
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-06-26 13:54:47 -0700

    adding post action to parse json request and send email using Pony gem, add pony gem and json to gemfile

--- email_api.rb
+++ email_api.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   "Hello World!"
+end
+
+post '/email' do
+  params = JSON.parse(request.env["rack.input"].read)  
+  Pony.mail :to => params["to"],
+              :from => "noreply@example.com",
+              :subject => params["subject"],
+              :body => params["body"]
 end
